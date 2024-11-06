82587fa8f6d08290a3a1695a173e1ffe77203fd0
Abhijit Pandey <abhijitpandey524@gmail.com>
2024-11-06 16:25:08 +0530

    setup email

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import { log } from "console";
+import mongoose, { mongo } from "mongoose";
+
+type ConnectionObject = {
+    isConnected? : number
+}
+
+const connection : ConnectionObject = {}
+
+async function dbConnect() : Promise<void> {
+
+    if(connection.isConnected){
+        console.log("already connected to db successfully")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || "" , {}) // try logging db object
+
+        connection.isConnected = db.connections[0].readyState
+        console.log("db connection successfully")
+    } catch (error) {
+        console.log("database connection unsuccessful")
+        process.exit(1)
+    }
+}
