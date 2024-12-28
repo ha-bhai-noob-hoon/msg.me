8dc4e61de7d3dc710e14f6f2c5be83bcb86d5cbd
Abhijit Pandey <abhijitpandey524@gmail.com>
2024-12-28 20:01:22 +0530

    homepage and minor fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
-import Navbar from "@/components/navbar";
+//import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>
-          <Navbar/>
+          
           {children}
         <Toaster />
         </body>
